2f1b30ee8d3beb6949f7eeb1ec8bb2bc47ac9239
AyushSingh1722 <138517505+AyushSingh1722@users.noreply.github.com>
2023-09-14 15:33:56 +0530

    Update custom_collate.py

--- custom_collate.py
+++ custom_collate.py
@@ -1,10 +1,22 @@
-import torch 
+# Import the PyTorch library
+import torch
 
-def collate_mismatch(batch): 
-    labels = [item[0] for item in batch]
-    imgs = [item[1] for item in batch] 
-    labels = torch.stack(labels,dim=0)
-    imgs = torch.stack(imgs, dim=0)
-    captions_batch = [item[2] for item in batch] 
+# Define a function called 'collate_mismatch' that takes a 'batch' of data as input
+def collate_mismatch(batch):
+    # Extract the labels, images, and captions from the batch
+    labels = [item[0] for item in batch]    # Extract labels (item[0]) from each item in the batch
+    imgs = [item[1] for item in batch]       # Extract images (item[1]) from each item in the batch
+
+    # Stack the labels and images to create tensors
+    labels = torch.stack(labels, dim=0)     # Stack the labels along dimension 0 to create a tensor
+    imgs = torch.stack(imgs, dim=0)         # Stack the images along dimension 0 to create a tensor
+
+    # Extract captions from each item in the batch
+    captions_batch = [item[2] for item in batch]
+
+    # Concatenate the captions to create a single tensor
     captions_batch = torch.cat(captions_batch, dim=0)
-    return labels, imgs, captions_batch 
+
+    # Return the labels, images, and captions as a tuple
+    return labels, imgs, captions_batch
+
