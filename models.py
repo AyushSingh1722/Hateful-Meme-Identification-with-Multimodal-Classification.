1f854e16b32da45bd68e6fb960721837f5ef148e
AyushSingh1722 <138517505+AyushSingh1722@users.noreply.github.com>
2023-09-14 15:48:32 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,4 @@
-"""
-Models class
-
-"""
-
+# Import necessary libraries and modules
 from sentence_transformers import SentenceTransformer, util
 import torch
 import torch.nn as nn
@@ -11,6 +7,7 @@ import torchvision
 from transformers import BertTokenizer, VisualBertModel
 from util import RPN
 
+# Define a class for the Baseline model
 class Baseline_model(nn.Module):
     """
     Baseline model for the Hateful Memes classification
@@ -19,38 +16,43 @@ class Baseline_model(nn.Module):
     from googlenet (torchvision) and fastText respectively
 
     Args:
-        hidden_size,
-        drop_prob
+        hidden_size: The size of the hidden layer
+        drop_prob: Dropout probability (default is 0.1)
     """
     
-    def __init__(self, hidden_size, drop_prob = 0.1):
+    def __init__(self, hidden_size, drop_prob=0.1):
         super(Baseline_model, self).__init__()
         
+        # Load a pre-trained vision model (ResNet-152)
         self.vision_pretrain = torchvision.models.resnet152(pretrained=True)
 
-        # pretrained transformers to get text embeddings
+        # Load a pre-trained text embedding model (SentenceTransformer)
         self.text_model = SentenceTransformer("all-mpnet-base-v2")
 
+        # Define linear layers and dropout for classification
         self.fc1 = nn.Linear(1768, hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, 1)
         self.dropout = nn.Dropout(drop_prob)
 
-
     def flatten(self, x):
-        N = x.shape[0] # read in N, C, H, W
-        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+        # Flatten the input tensor
+        N = x.shape[0]
+        return x.view(N, -1)
 
     def forward(self, image, text, device):
-        # concat 
+        # Pass the image through the vision model
         image = self.vision_pretrain(image)
+
+        # Encode the text using the text embedding model
         text = torch.tensor(self.text_model.encode(text)).squeeze().to(device)
 
+        # Flatten and concatenate the image and text features
         image = self.flatten(F.relu(image))
         text = self.flatten(F.relu(text))
-        combined_feat = torch.cat((image, text), dim = 1)
+        combined_feat = torch.cat((image, text), dim=1)
 
-        # forward through linear layers
+        # Pass the combined features through linear layers and apply dropout
         fc1_out = self.fc1(combined_feat)
         fc1_out = self.relu(fc1_out)        
         fc1_out = self.dropout(fc1_out)
@@ -59,25 +61,30 @@ class Baseline_model(nn.Module):
 
         return fc2_out
 
-
+# Define a class for the VisualBert model
 class VisualBert_Model(nn.Module):
     """
     Model that utilizes VisualBert
 
     Args:
-        hidden_size,
-        drop_prob
+        batch_size: Batch size
+        hidden_size: The size of the hidden layers
+        device: The device (CPU or GPU) for computation
+        drop_prob: Dropout probability (default is 0.1)
     """
-    def __init__(self, batch_size, hidden_size, device, drop_prob = 0.1):
+    def __init__(self, batch_size, hidden_size, device, drop_prob=0.1):
         super(VisualBert_Model, self).__init__()
-        self.device = device,
-        self.hidden = hidden_size,
+        self.device = device
+        self.hidden = hidden_size
 
+        # Load a pre-trained VisualBert model
         self.vbert_model = VisualBertModel.from_pretrained("uclanlp/visualbert-nlvr2-coco-pre")
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+        # Initialize the Region Proposal Network (RPN)
         self.RPN = RPN(batch_size, device)
         
+        # Define linear layers, layer normalization, and dropout for classification
         self.fc1 = nn.Linear(153600, hidden_size)
         self.relu1 = nn.LeakyReLU()
         self.fc2 = nn.Linear(hidden_size, hidden_size)
@@ -90,20 +97,20 @@ class VisualBert_Model(nn.Module):
         self.dropout2 = nn.Dropout(drop_prob)
 
     def flatten(self, x):
-        N = x.shape[0] # read in N, C, H, W
-        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+        # Flatten the input tensor
+        N = x.shape[0]
+        return x.view(N, -1)
 
     def forward(self, image, text, device):
-
         N, C, H, W = image.shape
 
-        # get visual embeddings
+        # Get visual embeddings using the Region Proposal Network (RPN)
         image_embeds = torch.stack(self.RPN.get_embeds(image))
         visual_token_type_ids = torch.ones(image_embeds.shape[:-1], dtype=torch.long).to(device)
         visual_attention_mask = torch.ones(image_embeds.shape[:-1], dtype=torch.float).to(device)
 
-        # tokenize and pad the text
-        inputs = self.tokenizer(text, padding='max_length', max_length=100, return_tensors = "pt").to(device)
+        # Tokenize and pad the text
+        inputs = self.tokenizer(text, padding='max_length', max_length=100, return_tensors="pt").to(device)
         inputs.update(
             {
                 "visual_embeds": image_embeds,
@@ -112,13 +119,14 @@ class VisualBert_Model(nn.Module):
             }
         )
 
-        # get last hidden of vbert
+        # Pass the inputs through the VisualBert model
         outputs = self.vbert_model(**inputs)
 
-        last_hidden_state = outputs.last_hidden_state # Bn, 150, 768
+        # Extract the last hidden state from VisualBert
+        last_hidden_state = outputs.last_hidden_state
         last_hidden_state = self.flatten(last_hidden_state)
 
-        # forward through linear layers
+        # Pass the hidden state through linear layers with normalization and dropout
         fc1_out = self.fc1(last_hidden_state)
         fc1_out = self.ln1(fc1_out)
         fc1_out = self.relu1(fc1_out)
@@ -131,3 +139,4 @@ class VisualBert_Model(nn.Module):
 
         fc3_out = self.fc3(fc2_out)
         return fc3_out
+
